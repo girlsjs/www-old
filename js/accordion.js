62bd3ed18d228b5eda1761bc5e42dd6da38f1c91
Artur Siery <dotintegral@gmail.com>
2016-09-05 20:26:17 +0200

    Fixing invalid accordion script

--- js/accordion.js
+++ js/accordion.js
@@ -3,21 +3,15 @@
   function accordionify() {
     var articles = document.querySelectorAll('article')
 
-    articles.forEach(function (article) {
+    Array.prototype.forEach.call(articles, function (article) {
       var headerContainer = article.querySelector('.header-container')
 
       headerContainer.addEventListener('click', function () {
-        // articles.forEach(function (a) {
-        //   a.classList.remove('open')
-        //   var c = a.querySelector('.content')
-        //   c.style.height = null
-        // })
-
         var content = article.querySelector('.content')
         var height = 0
 
         if (!article.classList.contains('open')) {
-          content.querySelectorAll('section').forEach(function (s) {
+          Array.prototype.forEach.call(content.querySelectorAll('section'), function (s) {
             height += s.offsetHeight
           })
         }
