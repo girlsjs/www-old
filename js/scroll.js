f1424f552e2d429166ac5aec370bda98143eb640
Artur Siery <dotintegral@gmail.com>
2016-09-10 16:09:46 +0200

    Mobile menu

--- js/scroll.js
+++ js/scroll.js
@@ -5,8 +5,36 @@
   }
 
   function onScroll(e){
+    var screenSize = window.screen.availWidth;
+
+    if (screenSize < 960) {
+      document.querySelector('header').style.height = ''
+      document.querySelector('.logo').style.transform = ''
+      return
+    }
+
+    var position = window.scrollY - 30;
+    // var initialHeight = (screenSize < 1220) ? 80 : 100
+    var initialHeight = 100;
+
+
+    var diff = initialHeight - position
+    diff = (diff < 50) ? 50 : diff
+
+    var scale = diff / initialHeight;
+    scale = (scale > 1) ? 1 : scale;
+
+    // var additionalScale = (screenSize < 1220) ? 0.7 : 1.0
+    // scale = scale * additionalScale
+
+    var headerHeight = (diff > initialHeight) ? initialHeight : diff
+
+    document.querySelector('header').style.height = headerHeight + 'px'
+    document.querySelector('.logo').style.transform = 'scale('+ scale +', '+ scale +')'
   }
 
   window.addEventListener("DOMContentLoaded", onLoad)
-  window.addEventListener("scroll", onScroll)
+  window.addEventListener("scroll", function () {
+    setTimeout(onScroll, 50)
+  })
 }())
