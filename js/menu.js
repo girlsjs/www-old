bdf831f82a82cc59f9f2b88a83db204424e75417
Arim Seventh <arimeq@gmail.com>
2016-12-04 23:48:29 +0100

    Photo gallery from last event.

--- js/menu.js
+++ js/menu.js
@@ -1,42 +1,32 @@
 (function () {
 
-  function onClick(e) {
-    e.preventDefault()
-
-    var link = e.currentTarget
-    var id = link.href.split('#')[1]
-    var pagePrefix = 'page-'
-    var nextUrl = pagePrefix + id
-
-    var prev = document.querySelector('article.visible')
-
+  function onHashChange(e) {
+    var id = e.newURL.split("#")[1];
+    var pagePrefix = "page-";
+    var nextUrl = pagePrefix + id;
+    var prev = document.querySelector("article.visible");
     if (prev.id === nextUrl) {
-      return
+      return;
     }
-
-    prev.classList.remove('visible')
-    var prevLink = document.querySelector('a[href="#' + prev.id.substr(pagePrefix.length) + '"]')
+    var link = document.querySelector("a[href='#" + id + "']");
+    var next = document.querySelector("#" + nextUrl);
+    if (!link || ! next) {
+      return;
+    }
+    prev.classList.remove("visible");
+    var prevLink = document.querySelector("a[href='#" + prev.id.substr(pagePrefix.length) + "']");
     if (prevLink) {
-      prevLink.classList.remove('active')
+      prevLink.classList.remove("active");
     }
-
-    var next = document.getElementById(nextUrl)
-    next.classList.add('visible')
-    link.classList.add('active')
-    location.hash = id
+    next.classList.add("visible");
+    link.classList.add("active");
   }
 
-
   function onLoad() {
-    var links = document.querySelectorAll('header nav a')
-    links = Array.prototype.slice.call(links)
-    links.push(document.querySelector(".logo"))
+    window.addEventListener("hashchange", onHashChange);
 
-    links.forEach(function (link) {
-      link.addEventListener("click", onClick)
-    })
     if (location.hash) {
-      document.querySelector('a[href="' + location.hash + '"]').click();
+      onHashChange({newURL: location.hash});
     }
   }
 
